8a3be8ca28c788eab1010c8d8b83e702dd1505b0
thdeathz <buidunga112@gmail.com>
2023-11-18 21:28:28 +0700

    chore(server): allow stagging url access api

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -2,5 +2,6 @@ export const allowedOrigins: string[] = [
   'http://localhost:3000',
   'http://localhost:5173',
   'http://project.localhost:3000',
-  'http://kanjigami.localhost:3000'
+  'http://kanjigami.localhost:3000',
+  'https://kanjigami.vercel.app/'
 ]
