8344db893f5af0c309fe6d5b75817936fd89e39b
thdeathz <buidunga112@gmail.com>
2023-11-21 22:00:52 +0700

    sprint-1-demo

--- server/src/config/corsOptions.ts
+++ server/src/config/corsOptions.ts
@@ -4,11 +4,7 @@ import { allowedOrigins } from './allowedOrigins'
 
 const corsOptions: CorsOptions = {
   origin: (origin: any, callback: Function) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
+    callback(null, true)
   },
   credentials: true,
   optionsSuccessStatus: 200
